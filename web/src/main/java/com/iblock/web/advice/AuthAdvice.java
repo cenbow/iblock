96b643218eb7c91a2c0c9cb358c6dbb9f4c37794
JerryBlight <qihong891009@gmail.com>
2016-07-24 21:24:17 +0800

    rate

--- web/src/main/java/com/iblock/web/advice/AuthAdvice.java
+++ web/src/main/java/com/iblock/web/advice/AuthAdvice.java
@@ -5,6 +5,7 @@ import com.iblock.web.controller.BaseController;
 import com.iblock.web.enums.ResponseStatus;
 import com.iblock.web.info.UserInfo;
 import com.iblock.web.response.CommonResponse;
+import lombok.extern.log4j.Log4j;
 import org.apache.commons.lang.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Aspect
+@Log4j
 public class AuthAdvice implements Ordered {
 
     @Around(value = "execution(* com.iblock.web.controller..*.*(..))")
@@ -28,6 +30,8 @@ public class AuthAdvice implements Ordered {
         Auth auth = methodSignature.getMethod().getAnnotation(Auth.class);
         if (auth != null && (user == null || (StringUtils.isNotBlank(auth.role()) && !auth.role().contains(String
                 .valueOf(user.getRole()))))) {
+            log.info(String.format("accessor has no auth, interface auth is %s and accessor role is %d", auth.role(),
+                    user.getRole()));
             return new CommonResponse<Boolean>(ResponseStatus.NO_AUTH);
         }
         return joinPoint.proceed();
