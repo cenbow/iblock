1162d5cb821dcab4110f1daf14e3747ffec3039c
JerryBlight <qihong891009@gmail.com>
2016-07-30 21:08:59 +0800

    fix

--- common/src/main/java/com/iblock/common/utils/MD5Utils.java
+++ common/src/main/java/com/iblock/common/utils/MD5Utils.java
@@ -1,6 +1,7 @@
 package com.iblock.common.utils;
 
 import com.sun.org.apache.xml.internal.security.utils.Base64;
+import sun.misc.BASE64Encoder;
 
 import java.security.MessageDigest;
 
@@ -17,12 +18,21 @@ public class MD5Utils {
             md.update(s.getBytes());
             // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
             // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-            System.out.println(s + " " + Base64.encode(md.digest()));
+            System.out.println(s + " " + getString(md.digest()));
             return new String(md.digest());
         } catch (Exception e) {
             e.printStackTrace();
         }
         return s;
+    }
+
+
+    private static String getString(byte[] b){
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < b.length; i ++){
+            sb.append(b[i]);
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
